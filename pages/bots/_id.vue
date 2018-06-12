882764d8b73299a19a0410eca8d478460208e033
Michael <iamovyerus@gmail.com>
2018-06-12 16:38:56 +1000

    Fix loading error on /bots/_id (#11)

--- pages/bots/_id.vue
+++ pages/bots/_id.vue
@@ -20,6 +20,37 @@ export default {
     };
   },
   middleware: "betaOnly",
+  asyncData(ctx) {
+    return ctx.$axios.$get(`/bots/${ctx.params.id}`, {
+      params: {
+        description: true,
+        owners: true,
+      }
+    }).then((bot) => {
+      let owners = "";
+      if(bot.owners) {
+        owners = bot.owners.map((owner) => `${owner.username}#${("000" + owner.discriminator).substring(owner.discriminator.toString().length - 1)}`).join(", ");
+      }
+      return {
+        bot: bot,
+        owners: owners,
+
+        canModify: !!bot.owners.find((owner) => owner.id === ctx.store.state.auth.user.id),
+        isMod: !!(ctx.store.state.auth.user.flags & 0b10000)
+      };
+    }).catch((err) => {
+      if(err && err.response && err.response.status) {
+        return ctx.error({
+          statusCode: err.response.status,
+          message: err.response.data && err.response.data.message || err.message || "Internal error"
+        });
+      }
+      return ctx.error({
+        statusCode: 500,
+        message: err.message || "Internal error"
+      });
+    });
+  },
   computed: {
     computedBot() {
       return Object.assign({}, this.bot, {owners: this.owners});
