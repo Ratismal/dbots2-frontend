171f948c251cdfba3a2bbea0f7f409fd67d43ad1
abalabahaha <hi@abal.moe>
2018-07-12 01:17:50 -0700

    Fix some bitflags

--- middleware/mod.js
+++ middleware/mod.js
@@ -2,7 +2,7 @@ export default function ({ store, redirect }) {
   if(!store.state.auth.user) {
     return redirect("/login");
   }
-  if(!(store.state.auth.user.flags & 0b10000)) {
+  if(!(store.state.auth.user.flags & 0b00000001000000000000000000000000)) {
     return redirect("/forbidden");
   }
 }
